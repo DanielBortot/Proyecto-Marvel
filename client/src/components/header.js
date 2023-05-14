fab5c1e14a72629a0082fa232572c757f980298f
DanielBortot <daniel.bortot@gmail.com>
2023-05-13 22:26:15 -0400

    reducers y bootstrap 5.0

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -1,19 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const style = {textDecoration: 'none', color: 'white', padding: '20px', display: 'inline-block', background: 'rgb(90, 74, 74)', marginRight: '15px'};
-
 function Header (){
     
     return (
         <div style={{background: 'rgb(233, 0, 0)', padding: '0 10px'}}>
             <li style={{listStyle: 'none'}}>
-                <Link style={style} to={'/'}>Inicio</Link>
-                <Link style={style} to={'/personajes'}>Personajes</Link>
-                <Link style={style} to={'/peliculas'}>Peliculas</Link>
-                <Link style={style} to={'/series'}>Series</Link>
-                <Link style={style} to={'/juegos'}>Juegos</Link>
-                <Link style={style} to={'/comics'}>Comics</Link>
+                <Link className='btn btn-secondary mx-4' style={{padding: '20px'}} to={'/'}>Inicio</Link>
+                <Link className='btn btn-secondary mx-4' style={{padding: '20px'}} to={'/personajes'}>Personajes</Link>
+                <Link className='btn btn-secondary mx-4' style={{padding: '20px'}} to={'/peliculas'}>Peliculas</Link>
+                <Link className='btn btn-secondary mx-4' style={{padding: '20px'}} to={'/series'}>Series</Link>
+                <Link className='btn btn-secondary mx-4' style={{padding: '20px'}} to={'/juegos'}>Juegos</Link>
+                <Link className='btn btn-secondary mx-4' style={{padding: '20px'}} to={'/comics'}>Comics</Link>
             </li>
         </div>
     );
