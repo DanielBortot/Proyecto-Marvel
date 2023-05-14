62d05eedcd5871e8eedbb405867d2a1d8f5c9903
DanielBortot <daniel.bortot@gmail.com>
2023-05-14 19:30:33 -0400

    bootstrap 5.0

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -4,14 +4,14 @@ import { Link } from "react-router-dom";
 function Header (){
     
     return (
-        <div style={{background: 'rgb(233, 0, 0)', padding: '0 10px'}}>
+        <div style={{background: 'rgb(233, 0, 0)', padding: '0 10px', position: 'sticky'}}>
             <li style={{listStyle: 'none'}}>
-                <Link className='btn btn-secondary mx-4' style={{padding: '20px'}} to={'/'}>Inicio</Link>
-                <Link className='btn btn-secondary mx-4' style={{padding: '20px'}} to={'/personajes'}>Personajes</Link>
-                <Link className='btn btn-secondary mx-4' style={{padding: '20px'}} to={'/peliculas'}>Peliculas</Link>
-                <Link className='btn btn-secondary mx-4' style={{padding: '20px'}} to={'/series'}>Series</Link>
-                <Link className='btn btn-secondary mx-4' style={{padding: '20px'}} to={'/juegos'}>Juegos</Link>
-                <Link className='btn btn-secondary mx-4' style={{padding: '20px'}} to={'/comics'}>Comics</Link>
+                <Link className='btn btn-secondary mx-4 px-4 py-4' to={'/'}>Inicio</Link>
+                <Link className='btn btn-secondary mx-4 px-4 py-4' to={'/personajes'}>Personajes</Link>
+                <Link className='btn btn-secondary mx-4 px-4 py-4' to={'/peliculas'}>Peliculas</Link>
+                <Link className='btn btn-secondary mx-4 px-4 py-4' to={'/series'}>Series</Link>
+                <Link className='btn btn-secondary mx-4 px-4 py-4' to={'/juegos'}>Juegos</Link>
+                <Link className='btn btn-secondary mx-4 px-4 py-4' to={'/comics'}>Comics</Link>
             </li>
         </div>
     );
