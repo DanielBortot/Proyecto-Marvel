2a776cae66b58d78410d5cafb1bd6bfa9f6c1968
DanielBortot <daniel.bortot@gmail.com>
2023-05-15 11:29:48 -0400

    rutas remove

--- server/src/controllers/acciones.js
+++ server/src/controllers/acciones.js
@@ -2,24 +2,49 @@ const pool = require('../database');
 
 const acciones = {
     personajes: async (req, res) => {
-        const personas = await pool.query('SELECT * FROM prueba');
-        res.send(personas.rows);
+        const personajes = await pool.query('SELECT * FROM prueba');
+        res.send(personajes.rows);
+    },
+    removePersonaje: async (req,res) => {
+        const id = req.params['id'];
+        await pool.query('DELETE * FROM prueba WHERE personaje_id = $1', [id]);
+        res.redirect('/api/personajes');
     },
     peliculas: async (req, res) => {
         const peliculas = await pool.query('SELECT * FROM prueba');
         res.send(peliculas.rows);
     },
+    removePelicula: async (req,res) => {
+        const id = req.params['id'];
+        await pool.query('DELETE * FROM prueba WHERE pelicula_id = $1', [id]);
+        res.redirect('/api/peliculas');
+    },
     series: async (req, res) => {
         const series = await pool.query('SELECT * FROM prueba');
         res.send(series.rows);
+    },
+    removeSerie: async (req,res) => {
+        const id = req.params['id'];
+        await pool.query('DELETE * FROM prueba WHERE series_id = $1', [id]);
+        res.redirect('/api/series');
     },
     juegos: async (req, res) => {
         const juegos = await pool.query('SELECT * FROM prueba');
         res.send(juegos.rows);
     },
+    removeJuego: async (req,res) => {
+        const id = req.params['id'];
+        await pool.query('DELETE * FROM prueba WHERE juego_id = $1', [id]);
+        res.redirect('/api/juegos');
+    },
     comics: async (req, res) => {
         const comics = await pool.query('SELECT * FROM prueba');
         res.send(comics.rows);
+    },
+    removeComic: async (req,res) => {
+        const id = req.params['id'];
+        await pool.query('DELETE * FROM prueba WHERE comic_id = $1', [id]);
+        res.redirect('/api/comics');
     }
 }
 
--- server/src/routes/rutas.js
+++ server/src/routes/rutas.js
@@ -1,10 +1,16 @@
 const router = require('express').Router();
-const {personajes, peliculas, series, juegos, comics} = require('../controllers/acciones');
+const {personajes, peliculas, series, juegos, comics, removePersonaje, removePelicula, removeSerie, removeJuego, removeComic} = require('../controllers/acciones');
 
 router.get('/personajes', personajes);
 router.get('/peliculas', peliculas);
 router.get('/series', series);
 router.get('/juegos', juegos);
 router.get('/comics', comics);
+
+router.post('removePersonaje/:id', removePersonaje);
+router.post('removePelicula/:id', removePelicula);
+router.post('removeSerie/:id', removeSerie);
+router.post('removeJuego/:id', removeJuego);
+router.post('removeComic/:id', removeComic);
 
 module.exports = router;
