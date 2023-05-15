3009f2835ce5e555dafeab5f6a75f207bf7ee30f
DanielBortot <daniel.bortot@gmail.com>
2023-05-15 12:31:10 -0400

    delete y post creados

--- server/src/controllers/acciones.js
+++ server/src/controllers/acciones.js
@@ -5,42 +5,81 @@ const acciones = {
         const personajes = await pool.query('SELECT * FROM prueba');
         res.send(personajes.rows);
     },
+
+    addPersonaje: async (req, res) => {
+        const {cont1, cont2 ,cont3} = req.body;
+        await pool.query('INSERT INTO prueba (cont1, cont2, cont3) VALUES ($1, $2, $3)', [cont1, cont2, cont3]);
+        res.redirect('/api/personajes');
+    },
+
     removePersonaje: async (req,res) => {
         const id = req.params['id'];
         await pool.query('DELETE * FROM prueba WHERE personaje_id = $1', [id]);
         res.redirect('/api/personajes');
     },
+
     peliculas: async (req, res) => {
         const peliculas = await pool.query('SELECT * FROM prueba');
         res.send(peliculas.rows);
     },
-    removePelicula: async (req,res) => {
-        const id = req.params['id'];
-        await pool.query('DELETE * FROM prueba WHERE pelicula_id = $1', [id]);
+
+    addPelicula: async (req, res) => {
+        const {cont1, cont2 ,cont3} = req.body;
+        await pool.query('INSERT INTO prueba (cont1, cont2, cont3) VALUES ($1, $2, $3)', [cont1, cont2, cont3]);
         res.redirect('/api/peliculas');
     },
+
+    removePelicula: async (req,res) => {
+        const id = req.params['id'];
+        await pool.query('DELETE FROM prueba WHERE pelicula_id = $1', [id]);
+        res.redirect('/api/peliculas');
+    },
+
     series: async (req, res) => {
         const series = await pool.query('SELECT * FROM prueba');
         res.send(series.rows);
     },
+
+    addSerie: async (req, res) => {
+        const {cont1, cont2 ,cont3} = req.body;
+        await pool.query('INSERT INTO prueba (cont1, cont2, cont3) VALUES ($1, $2, $3)', [cont1, cont2, cont3]);
+        res.redirect('/api/series');
+    },
+    
     removeSerie: async (req,res) => {
         const id = req.params['id'];
         await pool.query('DELETE * FROM prueba WHERE series_id = $1', [id]);
         res.redirect('/api/series');
     },
+
     juegos: async (req, res) => {
         const juegos = await pool.query('SELECT * FROM prueba');
         res.send(juegos.rows);
     },
+
+    addJuego: async (req, res) => {
+        const {cont1, cont2 ,cont3} = req.body;
+        await pool.query('INSERT INTO prueba (cont1, cont2, cont3) VALUES ($1, $2, $3)', [cont1, cont2, cont3]);
+        res.redirect('/api/juegos');
+    },
+
     removeJuego: async (req,res) => {
         const id = req.params['id'];
         await pool.query('DELETE * FROM prueba WHERE juego_id = $1', [id]);
         res.redirect('/api/juegos');
     },
+
     comics: async (req, res) => {
         const comics = await pool.query('SELECT * FROM prueba');
         res.send(comics.rows);
     },
+
+    addComic: async (req, res) => {
+        const {cont1, cont2 ,cont3} = req.body;
+        await pool.query('INSERT INTO prueba (cont1, cont2, cont3) VALUES ($1, $2, $3)', [cont1, cont2, cont3]);
+        res.redirect('/api/comics');
+    },
+    
     removeComic: async (req,res) => {
         const id = req.params['id'];
         await pool.query('DELETE * FROM prueba WHERE comic_id = $1', [id]);
--- server/src/routes/rutas.js
+++ server/src/routes/rutas.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const {personajes, peliculas, series, juegos, comics, removePersonaje, removePelicula, removeSerie, removeJuego, removeComic} = require('../controllers/acciones');
+const {personajes, peliculas, series, juegos, comics, removePersonaje, removePelicula, removeSerie, removeJuego, removeComic, addPersonaje, addPelicula, addSerie, addJuego, addComic} = require('../controllers/acciones');
 
 router.get('/personajes', personajes);
 router.get('/peliculas', peliculas);
@@ -7,10 +7,16 @@ router.get('/series', series);
 router.get('/juegos', juegos);
 router.get('/comics', comics);
 
-router.post('removePersonaje/:id', removePersonaje);
-router.post('removePelicula/:id', removePelicula);
-router.post('removeSerie/:id', removeSerie);
-router.post('removeJuego/:id', removeJuego);
-router.post('removeComic/:id', removeComic);
+router.delete('/removePersonaje/:id', removePersonaje);
+router.delete('/removePelicula/:id', removePelicula);
+router.delete('/removeSerie/:id', removeSerie);
+router.delete('/removeJuego/:id', removeJuego);
+router.delete('/removeComic/:id', removeComic);
+
+router.post('/addPersonaje', addPersonaje);
+router.post('/addPelicula', addPelicula);
+router.post('/addSerie', addSerie);
+router.post('/addJuego', addJuego);
+router.post('/addComic', addComic);
 
 module.exports = router;
