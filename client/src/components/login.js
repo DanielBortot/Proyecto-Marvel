aaf4dfa0f1452d0d47d06fb30bed969240f3e700
DanielBortot <daniel.bortot@gmail.com>
2023-06-04 20:10:09 -0400

    login

--- client/src/components/login.js
+++ client/src/components/login.js
@@ -1,10 +1,29 @@
 import React from "react";
+import '../assets/registro.css';
 
-function Login () {
+function Login() {
+
+    const enviar = e=> {
+        e.preventDefault();
+    }
 
     return (
-        <h1>Login</h1>
+        <>
+            <div className="tituloContReg">
+                <h2 className="titulo">Loguearse</h2>
+            </div>
+            <div className="formContReg">
+                <form>
+                    <input type="email" placeholder="Email"/>
+                    <input type="password" placeholder="Contraseña"/>
+                    <div className="botonReg" style={{marginTop: '30px'}}>
+                        <button type="submit" onClick={enviar}>Aceptar</button>
+                    </div>
+                </form>
+            </div>
+            
+        </>
     );
 }
 
-export {Login}
+export {Login};
