774a779b1c04afd2edec92e58a4fad4f1c27cb5e
DanielBortot <daniel.bortot@gmail.com>
2023-06-12 17:33:44 -0400

    registro terminado

--- client/src/components/register.js
+++ client/src/components/register.js
@@ -6,29 +6,37 @@ import { useDispatch } from "react-redux";
 import { datosUsuario } from "../reducers/usuarioSlice";
 import { Formik, Form, ErrorMessage, Field } from "formik";
 import { useNavigate } from "react-router-dom";
-import { setPais, setEstado } from "../reducers/direccionSlice";
+import { setPais, setEstado, setCiudad } from "../reducers/direccionSlice";
 
 function Register() {
     const [colores, setColores] = useState(['nselec','nselec','nselec','nselec']);
-    //const [erroresBD, setErroresBD] = useState({});
+    const [erroresBD, setErroresBD] = useState({});
     const [paises, setPaises] = useState([]);
     const [estados, setEstados] = useState([]);
     const [ciudades, setCiudades] = useState([]);
+    const [op, setOp] = useState([]);
     const dispatch = useDispatch();
     const navigate = useNavigate();
 
-    useEffect(async ()=>{
-        const ps = await (await axios.get('/paises')).data;
-        setPaises(ps);
+    useEffect(()=>{
+        const cargarPaises = async () => {
+            const ps = await (await axios.get('/api/paises')).data;
+            setPaises(ps);
+        }
+        cargarPaises();
     },[]);
 
     const paisRed = async (id) => {
+        console.log(id);
         paises.map(pais => {
             if (pais.Id_Pais === id){
                 dispatch(setPais(pais));
             }
         });
-        const estados = await (await axios.post('/estados',{Id_Pais: id})).data;
+        const list = op;
+        list[0] = id;
+        setOp(list);
+        const estados = await (await axios.post('/api/estados',{Id_Pais: id})).data;
         setEstados(estados);
     }
 
@@ -38,8 +46,19 @@ function Register() {
                 dispatch(setEstado(estado));
             }
         });
-        const ciudades = await (await axios.post('/ciudades', {Id_Estado: id})).data;
+        const list = op;
+        list[1] = id;
+        setOp(list);
+        const ciudades = await (await axios.post('/api/ciudades', {Id_Estado: id})).data;
         setCiudades(ciudades);
+    }
+
+    const ciudadRed = (id) => {
+        ciudades.map(ciudad => {
+            if (ciudad.Id_Ciudad === id){
+                dispatch(setCiudad(ciudad));
+            }
+        });
     }
 
     return (
@@ -80,20 +99,36 @@ function Register() {
                         if (!val.fecha){
                             errores.fecha = 'Introduzca la fecha de nacimiento';
                         }
-                        if (!val.pais){
+                        if (!val.pais || val.pais === 0){
                             errores.pais = 'Seleccione un pais';
+                        } else {
+                            if (op[0] !== val.pais){
+                                val.estado = 0;
+                                val.ciudad = 0;
+                            }
+                            paisRed(val.pais);
                         }
-                        if (!val.estado){
+                        if (!val.estado || val.estado === 0){
                             errores.estado = 'Seleccione un estado';
+                        } else {
+                            if (op[1] !== val.estado){
+                                val.ciudad = 0;
+                            }
+                            estadoRed(val.estado);
                         }
-                        if (!val.ciudad){
+                        if (!val.ciudad || val.ciudad === 0){
                             errores.ciudad = 'Seleccione una ciudad';
+                        } else {
+                            ciudadRed(val.ciudad);
                         }
+                        
                         return errores;
                     }}
                     onSubmit={async (val)=>{
-                        const erroresBD = (await axios.post('api/buscUsuario',{Email: val.email, Nombre: val.nombre, Apellido: val.apellido})).data;
+                        const erroresBD = await (await axios.post('api/buscUsuario',{Email: val.email, Nombre: val.nombre, Apellido: val.apellido})).data;
+                        setErroresBD(erroresBD);
                         const date = new Date();
+
                         if (!erroresBD.nombre && !erroresBD.apellido && !erroresBD.email){
                             const col = colores.findIndex(c => c ==='selec');
                             if (col === -1 || col === 3){
@@ -106,24 +141,22 @@ function Register() {
                                 dispatch(datosUsuario({Nombre: val.nombre, Apellido: val.apellido, Email: val.email, Contrasena: val.contra, Direccion: 3, N_Tarjeta: null, Id_Suscripcion: col+1, Fecha_Creacion: date.toLocaleDateString(), Fecha_Nac: val.fecha}));
                                 navigate('/registro/tarjeta');
                             }
-                        } else {
-                            alert(`${erroresBD.nombre}, ${erroresBD.apellido}, ${erroresBD.email}`);
                         }
                     }}
                 >
                     {({errors})=>(
                         <Form>
-                            {/* {erroresBD.nombre && <div style={{fontSize: "15px", color: "red"}}>{erroresBD.nombre}</div>} */}
+                            {erroresBD.nombre && <div style={{fontSize: "15px", color: "red"}}>{erroresBD.nombre}</div>}
                             <ErrorMessage name="nombre" id="nombre" component={()=> (<div style={{fontSize: "15px", color: "red"}}>{errors.nombre}</div>)}/>
 
                             <Field type="text" placeholder="Nombre" name="nombre"/>
 
-                            {/* {erroresBD.apellido && <div style={{fontSize: "15px", color: "red"}}>{erroresBD.apellido}</div>} */}
+                            {erroresBD.apellido && <div style={{fontSize: "15px", color: "red"}}>{erroresBD.apellido}</div>}
                             <ErrorMessage name="apellido" component={()=> (<div style={{fontSize: "15px", color: "red"}}>{errors.apellido}</div>)}/>
 
                             <Field type="text" placeholder="Apellido" name="apellido"/>
 
-                            {/* {erroresBD.email && <div style={{fontSize: "15px", color: "red"}}>{erroresBD.email}</div>} */}
+                            {erroresBD.email && <div style={{fontSize: "15px", color: "red"}}>{erroresBD.email}</div>}
                             <ErrorMessage name="email" component={()=> (<div style={{fontSize: "15px", color: "red"}}>{errors.email}</div>)}/>
                             
                             <Field type="email" placeholder="Email" name="email"/>
@@ -143,27 +176,27 @@ function Register() {
                             <ErrorMessage name="pais" id="pais" component={()=> (<div style={{fontSize: "15px", color: "red"}}>{errors.pais}</div>)}/>
 
                             <Field type="text" placeholder="Pais" name="pais" as="select">
-                            <option hidden selected>Selecciona el pais</option>
+                            <option hidden selected value={0}>Selecciona el pais</option>
                             {paises.map(pais => (
-                                <option value={pais.Id_Pais} onClick={()=>paisRed(pais.Id_Pais)}>{pais.Nombre}</option>
+                                <option key={pais.Id_Pais} value={pais.Id_Pais}>{pais.Nombre}</option>
                             ))}
                             </Field>
 
                             <ErrorMessage name="estado" id="estado" component={()=> (<div style={{fontSize: "15px", color: "red"}}>{errors.estado}</div>)}/>
 
                             <Field type="text" placeholder="Estado" name="estado" as="select">
-                            <option hidden selected>Selecciona el estado</option>
+                            <option hidden selected value={0}>Selecciona el estado</option>
                             {estados.map(estado => (
-                                <option value={estado.Id_Estado} onClick={()=>estadoRed(estado.Id_Estado)}>{estado.Nombre}</option>
+                                <option key={estado.Id_Estado} value={estado.Id_Estado} onClick={()=>estadoRed(estado.Id_Estado)}>{estado.Nombre}</option>
                             ))}
                             </Field>
 
                             <ErrorMessage name="ciudad" id="ciudad" component={()=> (<div style={{fontSize: "15px", color: "red"}}>{errors.ciudad}</div>)}/>
 
                             <Field type="text" placeholder="Ciudad" name="ciudad" as="select">
-                            <option hidden selected>Selecciona la ciudad</option>
+                            <option hidden selected value={0}>Selecciona la ciudad</option>
                             {ciudades.map(ciudad => (
-                                <option value={ciudad.Id_Ciudad}>{ciudad.Nombre}</option>
+                                <option key={ciudad.Id_Ciudad} value={ciudad.Id_Ciudad}>{ciudad.Nombre}</option>
                             ))}
                             </Field>      
                                 
