4865236b195d297d3769ea462207b0be3e257626
DanielBortot <daniel.bortot@gmail.com>
2023-06-14 19:35:05 -0400

    triggers

--- Base de datos/Insert_Ciudad_Correcto.sql
+++ Base de datos/Insert_Ciudad_Correcto.sql
@@ -5,3 +5,11 @@ INSERT INTO public."Ciudad"(
 INSERT INTO public."Ciudad"(
 	"Id_Ciudad", "Nombre", "Id_Estado")
 	VALUES (112, 'Ciudad de Bogota', 12);
+
+INSERT INTO public."Ciudad"(
+	"Id_Ciudad", "Nombre", "Id_Estado")
+	VALUES (113, 'Los Teques', 11);
+
+INSERT INTO public."Ciudad"(
+	"Id_Ciudad", "Nombre", "Id_Estado")
+	VALUES (114, 'Caldas', 12);
--- Base de datos/Insert_Usuario_Correcto.sql
+++ Base de datos/Insert_Usuario_Correcto.sql
@@ -1,20 +1,20 @@
 INSERT INTO public."Usuario"(
 	"Email", "Contrasena", "Nombre", "Apellido", "Fecha_Nac", "Fecha_Creacion", "Id_Suscripcion", "Direccion", "N_Tarjeta")
-	VALUES ('usu1@ejemplo.com', '1234', 'nombre_usu1', 'apellido_usu1', '01/01/1990', '01/01/2023', 4, 111, null);
+	VALUES ('usu1@ejemplo.com', '1234', 'nombre_usu1', 'apellido_usu1', '01/01/1990', '01/01/2023', 3, 111, null);
 
 	
 INSERT INTO public."Usuario"(
 	"Email", "Contrasena", "Nombre", "Apellido", "Fecha_Nac", "Fecha_Creacion", "Id_Suscripcion", "Direccion", "N_Tarjeta")
-	VALUES ('usu2@ejemplo.com', '5678', 'nombre_usu2', 'apellido_usu2', '01/01/1990', '01/01/2023', 3, 111, null);
+	VALUES ('usu7@ejemplo.com', '5678', 'nombre_usu7', 'apellido_usu7', '01/01/1990', '01/01/2023', 4, 111, null);
 	
 
 INSERT INTO public."Usuario"(
 	"Email", "Contrasena", "Nombre", "Apellido", "Fecha_Nac", "Fecha_Creacion", "Id_Suscripcion", "Direccion", "N_Tarjeta")
-	VALUES ('usu3@ejemplo.com', '8765', 'nombre_usu3', 'apellido_usu3', '01/01/1990', '01/01/2023', 4, 111, null);
+	VALUES ('usu3@ejemplo.com', '8765', 'nombre_usu3', 'apellido_usu3', '01/01/1990', '01/01/2023', 3, 111, null);
 	
 INSERT INTO public."Usuario"(
 	"Email", "Contrasena", "Nombre", "Apellido", "Fecha_Nac", "Fecha_Creacion", "Id_Suscripcion", "Direccion", "N_Tarjeta")
-	VALUES ('usu4@ejemplo.com', '4321', 'nombre_usu4', 'apellido_usu4', '01/01/1990', '01/01/2023', 4, 112, null);
+	VALUES ('usu4@ejemplo.com', '4321', 'nombre_usu4', 'apellido_usu4', '01/01/1990', '01/01/2023', 3, 112, null);
 	
 INSERT INTO public."Usuario"(
 	"Email", "Contrasena", "Nombre", "Apellido", "Fecha_Nac", "Fecha_Creacion", "Id_Suscripcion", "Direccion", "N_Tarjeta")
@@ -22,5 +22,5 @@ INSERT INTO public."Usuario"(
 	
 INSERT INTO public."Usuario"(
 	"Email", "Contrasena", "Nombre", "Apellido", "Fecha_Nac", "Fecha_Creacion", "Id_Suscripcion", "Direccion", "N_Tarjeta")
-	VALUES ('usu6@ejemplo.com', '1020', 'nombre_usu6', 'apellido_usu6', '01/01/1990', '01/01/2023', 4, 111, null);
+	VALUES ('usu6@ejemplo.com', '1020', 'nombre_usu6', 'apellido_usu6', '01/01/1990', '01/01/2023', 3, 111, null);
 		
