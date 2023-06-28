e07499d48ddff4b47a0d0d5f28cb0e632f226987
Rhetium <marcoagrusa@gmail.com>
2023-06-27 22:57:06 -0400

    enter

--- Base de datos/Reportes/Reporte_6.sql
+++ Base de datos/Reportes/Reporte_6.sql
@@ -1,4 +1,4 @@
-SELECT Pd.*
+SELECT Pd."Nombre",Pd."Imagen",Pd."Descripcion"
 FROM "Posee" AS Po JOIN "Poder" AS Pd ON (Po."N_Poder" = Pd."Nombre")
 					JOIN "Villano" AS V ON (Po."N_Personaje" = V."N_Villano")
 WHERE (Po."Obtencion" = 'Hereditario') AND (Po."N_Poder" LIKE  'Super%')
