617c2442e53308269e04b7cdb99c6e65b8b2ea9e
Rhetium <marcoagrusa@gmail.com>
2023-06-27 21:36:54 -0400

    enter

---
+++ client/src/assets/img/imgdb.js
@@ -0,0 +1,145 @@
+import img1 from "./abominacion.jpg"
+import img2 from "./antmanpelicula.jpg"
+import img3 from "./av2.jpg"
+import img4 from "./avengers.jpg"
+import img5 from "./avengers2019.jpg"
+import img6 from "./avengerspelicula.jpg"
+import img7 from "./blackwidow.jpg"
+import img8 from "./blackwidow2021.jpg"
+import img9 from "./cap2.jpg"
+import img10 from "./capcivilwar.jpg"
+import img11 from "./capitanamerica.jpg"
+import img12 from "./cappelicula.jpg"
+import img13 from "./craneorojo.jpg"
+import img14 from "./daredevil.jpg"
+import img15 from "./daredevilserie.jpg"
+import img16 from "./defendersserie.jpg"
+import img17 from "./drax.jpg"
+import img18 from "./eitri.jpg"
+import img19 from "./fury.jpg"
+import img20 from "./gamora.jpg"
+import img21 from "./groot.jpg"
+import img22 from "./guardianes.jpg"
+import img23 from "./guardianespelicula.jpg"
+import img24 from "./hawkeye.jpg"
+import img25 from "./hela.jpg"
+import img26 from "./howardstark.jpg"
+import img27 from "./hulk.png"
+import img28 from "./hulk2008.jpg"
+import img29 from "./hydra.jpg"
+import img30 from "./ironfist.jpg"
+import img31 from "./ironfistserie.jpg"
+import img32 from "./ironman.jpg"
+import img33 from "./ironman2_2010.jpg"
+import img34 from "./ironman3_2013.jpg"
+import img35 from "./ironman2008.jpg"
+import img36 from "./ironmansurgeseltecnivoro.jpg"
+import img37 from "./ironnonger.jpg"
+import img38 from "./jessjones.jpg"
+import img39 from "./jessjonesserie.jpg"
+import img40 from "./kingpin.png"
+import img41 from "./loki.jpg"
+import img42 from "./lukecage.jpg"
+import img43 from "./lukecageserie.jpg"
+import img44 from "./mariahill.jpg"
+import img45 from "./namor.jpg"
+import img46 from "./nebula.jpg"
+import img47 from "./octopus.jpg"
+import img48 from "./profesorx.jpg"
+import img49 from "./punisher.jpg"
+import img50 from "./punisherserie.jpg"
+import img51 from "./rocket.jpg"
+import img52 from "./ronan.jpg"
+import img53 from "./sharoncarter.jpg"
+import img54 from "./shield.jpg"
+import img55 from "./spiderman.jpg"
+import img56 from "./spiderman2_2004.jpg"
+import img57 from "./spidermanmultiverse.jpg"
+import img58 from "./starlord.jpg"
+import img59 from "./taskmaster.jpg"
+import img60 from "./thor.jpg"
+import img61 from "./thor2.jpg"
+import img62 from "./thor2017.jpg"
+import img63 from "./thorpelicula.jpg"
+import img64 from "./tormenta.jpg"
+import img65 from "./ultimateav2.jpg"
+import img66 from "./wakandaforever.jpg"
+import img67 from "./wolverine.jpg"
+import img68 from "./xmen.jpg"
+import img69 from "./xmen2000.jpg"
+import img70 from "./zemo.jpg"
+
+const imagenes = [
+    {img : img1, pos: 1},
+    {img : img2, pos: 2},
+    {img : img3, pos: 3},
+    {img : img4, pos: 4},
+    {img : img5, pos: 5},
+    {img : img6, pos: 6},
+    {img : img7, pos: 7},
+    {img : img8, pos: 8},
+    {img : img9, pos: 9},
+    {img : img10, pos: 10},
+    {img : img11, pos: 11},
+    {img : img12, pos: 12},
+    {img : img13, pos: 13},
+    {img : img14, pos: 14},
+    {img : img15, pos: 15},
+    {img : img16, pos: 16},
+    {img : img17, pos: 17},
+    {img : img18, pos: 18},
+    {img : img19, pos: 19},
+    {img : img20, pos: 20},
+    {img : img21, pos: 21},
+    {img : img22, pos: 22},
+    {img : img23, pos: 23},
+    {img : img24, pos: 24},
+    {img : img25, pos: 25},
+    {img : img26, pos: 26},
+    {img : img27, pos: 27},
+    {img : img28, pos: 28},
+    {img : img29, pos: 29},
+    {img : img30, pos: 30},
+    {img : img31, pos: 31},
+    {img : img32, pos: 32},
+    {img : img33, pos: 33},
+    {img : img34, pos: 34},
+    {img : img35, pos: 35},
+    {img : img36, pos: 36},
+    {img : img37, pos: 37},
+    {img : img38, pos: 38},
+    {img : img39, pos: 39},
+    {img : img40, pos: 40},
+    {img : img41, pos: 41},
+    {img : img42, pos: 42},
+    {img : img43, pos: 43},
+    {img : img44, pos: 44},
+    {img : img45, pos: 45},
+    {img : img46, pos: 46},
+    {img : img47, pos: 47},
+    {img : img48, pos: 48},
+    {img : img49, pos: 49},
+    {img : img50, pos: 50},
+    {img : img51, pos: 51},
+    {img : img52, pos: 52},
+    {img : img53, pos: 53},
+    {img : img54, pos: 54},
+    {img : img55, pos: 55},
+    {img : img56, pos: 56},
+    {img : img57, pos: 57},
+    {img : img58, pos: 58},
+    {img : img59, pos: 59},
+    {img : img60, pos: 60},
+    {img : img61, pos: 61},
+    {img : img62, pos: 62},
+    {img : img63, pos: 63},
+    {img : img64, pos: 64},
+    {img : img65, pos: 65},
+    {img : img66, pos: 66},
+    {img : img67, pos: 67},
+    {img : img68, pos: 68},
+    {img : img69, pos: 69},
+    {img : img70, pos: 70}
+    
+ ]
+export {imagenes}
