ee27a2f2f1a71b7d05fbc493444a3ccef2760243
DanielBortot <daniel.bortot@gmail.com>
2023-07-15 15:49:57 -0400

    reportes

--- client/src/components/Reportes/Reporte1.js
+++ client/src/components/Reportes/Reporte1.js
@@ -1,18 +1,10 @@
 import React, { useEffect, useState } from "react";
 import '../../assets/Reporte2.css';
-import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
-import { useDispatch } from "react-redux";
-import { datosReporte } from "../../reducers/reportesSlice";
-import { faTrash } from '@fortawesome/free-solid-svg-icons'
-import { faPenToSquare} from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function Reporte1 (){
 
     const [reporte, setReporte] = useState([]);
-    const dispatch = useDispatch();
-    const navigate = useNavigate();
 
     const traerDatos = async ()=> {
         const datos = await (await axios.get('/api/persRep1')).data;
@@ -23,34 +15,16 @@ function Reporte1 (){
         traerDatos();
     },[])
 
-    const eliminar = async (id) => {
-        await axios.post('/api/delRep2', {T_Serie: id});
-        let lista = [...reporte];
-        lista = lista.filter(rep => rep.Titulo !== id);
-        setReporte(lista);
-        traerDatos();
-    }
-
-    const update = async (val) => {
-        dispatch(datosReporte(val));
-        navigate('modRep2');
-    }
-
     return (
         <>  
             <div className="rep2">
                 <h3>Heroes o Villanos con poderes artificiales que han sido lideres.</h3>
-                <Link className='btn btn-danger' style={{margin: '15px 0 10px 15px'}} to={'addRep2'}>Crear Campo</Link>
                 <div className="rep2Cont">
                     <div className="rep2item"><span style={{fontWeight: 'bold'}}>Personaje</span></div>
                     <div className="rep2item"><span style={{fontWeight: 'bold'}}>Organización</span></div>
                     {reporte.map(rep => (
                         <>
                             <div className="rep2ContDato" key={rep.titulo}>
-                                <div className="rep2ContIcon">
-                                    <FontAwesomeIcon icon={faTrash} onClick={()=> eliminar(rep.titulo)} style={{padding: '5px', cursor: 'pointer'}}/>
-                                    <FontAwesomeIcon icon={faPenToSquare} style={{padding: '5px', cursor: 'pointer'}} onClick={()=> update(rep)}/>
-                                </div>
                                 <div className="rep2item">{rep.Nombre}</div>
                             </div>
                             <div className="rep2item">{rep.N_Organizacion}</div>
--- client/src/components/Reportes/Reporte5.js
+++ client/src/components/Reportes/Reporte5.js
@@ -1,18 +1,10 @@
 import React, { useEffect, useState } from "react";
 import '../../assets/Reporte5.css';
-import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
-import { datosReporte } from "../../reducers/reportesSlice";
-import { useDispatch } from "react-redux";
-import { faTrash } from '@fortawesome/free-solid-svg-icons'
-import { faPenToSquare} from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function Reporte5 (){
 
     const [reporte, setReporte] = useState([]);
-    const dispatch = useDispatch();
-    const navigate = useNavigate();
 
     const traerDatos = async ()=> {
         const datos = await (await axios.get('/api/Rep5Peliculas')).data;
@@ -24,24 +16,10 @@ function Reporte5 (){
         traerDatos();
     },[]);
 
-    const eliminar = async (id) => {
-        await axios.post('/api/delRep5', {T_Pelicula: id});
-        let lista = [...reporte];
-        lista = lista.filter(rep => rep.Titulo !== id);
-        setReporte(lista);
-        traerDatos();
-    }
-
-    const update = async (val) => {
-        dispatch(datosReporte(val));
-        navigate('ModRep5');
-    }
-
     return (
         <>  
             <div className="rep5">
                 <h3>Películas que tengan más de 2 horas y media de duración, sean de tipo animada, cuya ganancia sea mayor al promedio de todas las películas del mismo tipo</h3>
-                <Link className='btn btn-danger' style={{margin: '15px 0 10px 15px'}} to={'addRep5'}>Crear Pelicula</Link>
                 <div className="rep5Cont">
                     <div className="rep5item"><span style={{fontWeight: 'bold'}}>Película</span></div>
                     <div className="rep5item"><span style={{fontWeight: 'bold'}}>Duración</span></div>
@@ -51,10 +29,6 @@ function Reporte5 (){
                     {reporte.map(rep => (
                         <>
                             <div className="rep5ContDato" key={rep.titulo}>
-                                <div className="rep5ContIcon">
-                                    <FontAwesomeIcon icon={faTrash} onClick={()=> eliminar(rep.Titulo)} style={{padding: '5px', cursor: 'pointer'}}/>
-                                    <FontAwesomeIcon icon={faPenToSquare} style={{padding: '5px', cursor: 'pointer'}} onClick={()=> update(rep)}/>
-                                </div>
                                 <div className="rep5item">{rep.Titulo}</div>
                             </div>
                             <div className="rep5item">{rep.Duracion}</div>
