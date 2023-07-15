52184f5fe250966e877fda46bfae167815ecdae5
Marco Agrusa <marcoagrusa@gmail.com>
2023-07-15 16:01:08 -0400

    enter

--- Base de datos/Reportes/Reporte_8.sql
+++ Base de datos/Reportes/Reporte_8.sql
@@ -1,9 +1,7 @@
-SELECT *
-FROM "Personaje"
-WHERE "Nombre" IN (SELECT "N_Personaje" FROM "Esta"
-				  	WHERE "N_Titulo" IN (SELECT Me."Titulo" FROM "Medio" AS Me JOIN "Pelicula" AS  Pe ON (Me."Titulo" = Pe."T_Pelicula")																 			   
-										 WHERE (Pe."Tipo" = 'Animacion'))
-				  						 OR "N_Titulo" IN (SELECT Me."Titulo" FROM "Medio" AS Me JOIN "Serie" AS  S ON (Me."Titulo" = S."T_Serie")
-										 WHERE (S."Tipo" = 'Animacion')))
-	  AND "Nombre" IN (SELECT "N_Heroe" FROM "Heroe")
-	  AND "Genero" = 'F';
+SELECT "N_Personaje", "N_Titulo" FROM "Esta"
+WHERE ("N_Titulo" IN (SELECT Me."Titulo" FROM "Medio" AS Me JOIN "Pelicula" AS  Pe ON (Me."Titulo" = Pe."T_Pelicula")																 			   
+					 WHERE (Pe."Tipo" = 'Animacion'))
+	  OR "N_Titulo" IN (SELECT Me."Titulo" FROM "Medio" AS Me JOIN "Serie" AS  S ON (Me."Titulo" = S."T_Serie")
+						WHERE (S."Tipo" = 'Animacion')))
+	  AND ("N_Personaje" IN (SELECT "N_Heroe" FROM "Heroe"))
+	  AND ("N_Personaje" IN (SELECT "Nombre" FROM "Personaje" WHERE "Genero" = 'F'));
